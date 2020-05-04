25b4f2ed2505c6211ec02af265b516cb5aa3a5cf
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-05-04 14:04:41 +0800

    clean code

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,12 +1,13 @@
 import os
 
 from system_information import System_Information as machine
-from system_tools import Tools as tool
-
 
 print('\n-----Computer Usage Monitoring-----\n')
 print('Computer name: {name}'.format(name=machine.get_machine_name()))
+print('Operating System: {os} {version}'.format(
+    os=machine.get_operating_system(), version=machine.get_machine_version()))
+print('Computer Processor: {cpu}'.format(cpu=machine.get_processor()))
 print('IP Adddress: {ip}'.format(ip=machine.get_ip_address()))
-print('Disk Usage: {disk_usage}%'.format(disk_usage=tool.get_disk_usage()))
-print('Memory Usage: {memory}%'.format(memory=tool.get_memory_usage()))
-print('CPU Usage: {cpu}%\n'.format(cpu=tool.get_cpu_usage()))
+print('Disk Usage: {disk_usage}%'.format(disk_usage=machine.get_disk_usage()))
+print('Memory Usage: {memory}%'.format(memory=machine.get_memory_usage()))
+print('CPU Usage: {cpu}%\n'.format(cpu=machine.get_cpu_usage()))
--- apps/system_information.py
+++ apps/system_information.py
@@ -1,6 +1,8 @@
 import socket
+import platform
+from system_tools import Tools
 
-class System_Information:
+class System_Information(Tools):
 
     def get_machine_name():
         return socket.gethostname()
@@ -8,3 +10,12 @@ class System_Information:
     @classmethod
     def get_ip_address(cls):
         return socket.gethostbyname(cls.get_machine_name())
+
+    def get_machine_version():
+        return platform.version()
+
+    def get_operating_system():
+        return platform.system()
+
+    def get_processor():
+        return platform.processor()
